ffde6f20b889feb1ab3629325fb2b909a96f9210
Cightline <sightline.networks@gmail.com>
2017-05-25 13:58:36 -0500

    removed extra check

--- libs/voat_rest/authenticate.py
+++ libs/voat_rest/authenticate.py
@@ -15,19 +15,6 @@ class Authenticate(Resource):
         args = parser.parse_args()
 
 
-        if 'username' not in args:
-            return {'error':'no username given'}
-
-        elif 'password' not in args:
-            return {'error':'no password given'}
-
-        elif args['password'] == None or args['password'] == '':
-            return {'error':'password has no length'}
-
-        elif args['username'] == None or args['username'] == '':
-            return {'error':'username has no length'}
-
-
         result, user = user_utils.authenticate_by_password(args['username'], args['password'])
 
         if result == False:
