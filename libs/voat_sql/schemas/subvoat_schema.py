526ddec43fd3a525588611165d1db970c82963b3
Cightline <sightline.networks@gmail.com>
2017-05-28 18:51:51 -0500

    Update subvoat_schema.py

--- libs/voat_sql/schemas/subvoat_schema.py
+++ libs/voat_sql/schemas/subvoat_schema.py
@@ -6,8 +6,7 @@ from voat_sql import *
 
 class SubVoat(Base):
     __tablename__ = 'subvoat'
-
-
+    
     id            = Column(Integer, primary_key=True)
     name          = Column(String(200), unique=True, nullable=False)
     threads       = relationship('Thread', backref=backref('thread', lazy='noload'))
@@ -19,7 +18,6 @@ class SubVoat(Base):
 class Thread(Base):
     __tablename__ = 'thread'
     
-    # NEED A CUSTOM COLUMN TYPE FOR UUIDs
     uuid          = Column(String(200), primary_key=True)
     title         = Column(String(200))
     body          = Column(String(200))
