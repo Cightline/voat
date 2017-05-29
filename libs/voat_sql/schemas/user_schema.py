34d5157ec1e045cbac4e84431da45266139ba29c
Cightline <sightline.networks@gmail.com>
2017-05-29 17:45:14 -0500

    small change

--- libs/voat_sql/schemas/user_schema.py
+++ libs/voat_sql/schemas/user_schema.py
@@ -7,8 +7,6 @@ class User(Base):
     creation_time = Column(DateTime)
     username      = Column(String(200), unique=True, nullable=False)
     email         = Column(String(200), unique=True)
-    public_key    = Column(String(200))
-    private_key   = Column(String(200))
     password_hash = Column(String(200))
     api_token     = Column(String(200))
     banned        = Column(Boolean)
